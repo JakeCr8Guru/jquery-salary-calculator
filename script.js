db96d12599a6cf96a282c4f854debd71a127cbdc
Jake Schaffer <jakecr8guru@gmail.com>
2020-05-23 17:00:27 -0500

    add addingEmployeesIntoArray function

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ function hereWeGo() {
 // - split up functionality into functions
 // - create a function that takes inputs and pushes an object to an array
 
+// GLOBAL VARIABLES! ----------------------------------------------
+//
+// From the DOM in jQuery =========================================
 // Input variables with values:
 const firstNameInputValue = $('#first-name').val();
 const lastNameInputValue = $('#last-name').val();
@@ -20,10 +23,21 @@ const titleInputValue = $('#title').val();
 const annualSalaryInputValue = $('#annual-salary').val();
 // Input submit button variable:
 const inputSubitionButton = $('#submit-btn');
+// End of jQuery global variables ================================
+//
+// Straight JavaScript variables =================================
 // Array where employee objects will be stored
 const addedEmployeeArray = [];
+// End of JavaScript global variables ============================
+//
+// End of GLOBAL VARIABLES ---------------------------------------
 
+
+// JAVSCRIPT FUNCTIONS! ----------------------------------------------------------------------
+// ~ addingEmployeesIntoArray function ~
+// - Straight JavaScript takes 5 parameters and creates an object then pushes object to global array
 function addingEmployeesIntoArray(firstName, lastName, iD, title, annualSalary) {
+    console.log('In addingEmployeesIntoArray function!');
     const employeeNew = {
         first: firstName,
         last: lastName,
@@ -33,3 +47,4 @@ function addingEmployeesIntoArray(firstName, lastName, iD, title, annualSalary) 
     };
     addedEmployeeArray.push(employeeNew);
 }
+// ~ End of addingEmployeesIntoArray function ~
