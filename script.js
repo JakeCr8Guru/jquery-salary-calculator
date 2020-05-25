6631d0415c0991f4be7c53fb606f8d3aecaffb3d
Jake Schaffer <jakecr8guru@gmail.com>
2020-05-25 11:02:32 -0500

    cleaning up script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 $(document).ready(hereWeGo);
-// fucntion that starts the whole thing
-// hereWeGo function
+
+// ~ hereWeGo function ~
+// - fucntion that starts the whole thing
 function hereWeGo() {
     console.log('In hereWeGo function!');
     inputSubmissionClickHandler();
     deleteEmployeeClickHandler();
     monthlyTotalCalDOM();
 }
+// ~ End of hereWeGo function ~
+
 // Array where employee objects will be stored
 const addedEmployeeArray = [];
-// End of JavaScript global variables ============================
-// JAVSCRIPT FUNCTIONS! ----------------------------------------------------------------------
+
 // ~ addingEmployeesIntoArray function ~
 // - Straight JavaScript takes 5 parameters and creates an object then pushes object to global array
 function addingEmployeesIntoArray(firstName, lastName, iD, title, annualSalary) {
@@ -26,10 +28,7 @@ function addingEmployeesIntoArray(firstName, lastName, iD, title, annualSalary) 
     return true;
 }
 // ~ End of addingEmployeesIntoArray function ~
-//
-// End of JAVSCRIPT FUNCTIONS -----------------------------------------------------------------
-// JQUERY FUNCTIONS! --------------------------------------------------------------------------
-//
+
 // ~ addInputsIntoArray function ~
 // - jQuery variables from DOM inputs that are placed in as parameters into addingEmployeesIntoArray function
 function addInputsIntoArray() {
@@ -40,6 +39,7 @@ function addInputsIntoArray() {
     const iDInputValue = Number($('#id').val());
     const titleInputValue = $('#title').val();
     const annualSalaryInputValue = Number($('#annual-salary').val());
+    // If Conditional that checks if inputs are correct types:
     if (
         firstNameInputValue !== undefined && firstNameInputValue !== null && firstNameInputValue !== '' &&
         lastNameInputValue !== undefined && lastNameInputValue !== null && lastNameInputValue !== '' &&
@@ -47,7 +47,7 @@ function addInputsIntoArray() {
         iDInputValue !== undefined && iDInputValue !== null && iDInputValue !== '' && !isNaN(iDInputValue) && iDInputValue !== 0 &&
         annualSalaryInputValue !== undefined && annualSalaryInputValue !== null && annualSalaryInputValue !== '' && !isNaN(annualSalaryInputValue) && annualSalaryInputValue !== 0
         ) {
-        // Pass in variables of inputs in:
+        // Passes variables of inputs in:
         addingEmployeesIntoArray(
         	firstNameInputValue,
         	lastNameInputValue,
@@ -67,6 +67,7 @@ function addInputsIntoArray() {
     $('#title').val('');
     $('#annual-salary').val('');
     // Run this after:
+    // Appends values of inputs into table row data cells:
     let tableRowIs = $(`<tr class="delete-me"></tr>`);
     $('.table-body').append(tableRowIs);
     tableRowIs.append(`<td>${firstNameInputValue}</td>`);
@@ -75,12 +76,13 @@ function addInputsIntoArray() {
     tableRowIs.append(`<td>${titleInputValue}</td>`);
     tableRowIs.append(`<td class="annSal">${annualSalaryInputValue}</td>`);
     tableRowIs.append('<td><button class="delete-employee">Clear</button></td>');
-
+    // Runs these functions:
     monthlyTotalCalDOM();
     deleteEmployeeClickHandler();
 
 }
 // ~ End of addInputsIntoArray function ~
+
 // ~ inputSubmissionClickHandler function ~
 // - on click this function will run addInputsIntoArray function so inputs will be added to array
 function inputSubmissionClickHandler() {
@@ -88,8 +90,11 @@ function inputSubmissionClickHandler() {
     const inputSubmissionButton = $('#submit-btn');
     inputSubmissionButton.click(addInputsIntoArray);
 }
-// End of inputSubmissionClickHandler function
-//
+// ~ End of inputSubmissionClickHandler function ~
+
+// ~ deleteEmployeeClickHandler ~
+// - on click this function will delete the row that it is in and splice the monthly salary index of which it is in the monthlyTotal array
+// - HAS BUGS...
 function deleteEmployeeClickHandler() {
     console.log('In deleteEmployeeClickHandler function!');
     let deleteBtn = $('.delete-employee')
@@ -109,16 +114,19 @@ function deleteEmployeeClickHandler() {
         }
         $(this).closest('tr').remove();
     });
-    
 }
+// ~ End of deleteEmployeeClickHandler ~
 
+// Variables for calculations
 let monthlyTotal = [];
 let sumOfArray = 0;
 let totalIs = 0;
 let number = 0;
+
+// ~ monthlyTotalCalDOM function ~
+// -
 function monthlyTotalCalDOM() {
     console.log('In monthlyTotalCalDOM function!');
-    
     $('.table-body tr').children('.annSal').each(function () {
         let element1 = $(this).text();
         // http://jsfiddle.net/jinglesthula/hdzTy
@@ -143,4 +151,4 @@ function monthlyTotalCalDOM() {
         element2.append(new Intl.NumberFormat().format(monthlySals));
     }
 }
-
+// ~ End of monthlyTotalCalDOM function ~
